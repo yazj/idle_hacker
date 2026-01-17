cb7f998a4123fa8dde12d110442775ccc9c24a9a
yuanyu <yuanyi339@gmail.com>
2026-01-17 23:43:49 +0800

    edit file terminal.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include <spdlog/spdlog.h>
 
 int main() {
+
+  // init spdlog
   auto console = spdlog::stdout_color_mt("console");
   spdlog::set_default_logger(console);
 
   spdlog::set_level(spdlog::level::debug);
-
-  spdlog::info("Welcome to idle_hacker");
-  spdlog::error("Oh it's so cool");
 
   return 0;
 }
---
+++ src/terminal/terminal.hpp
@@ -0,0 +1,60 @@
+/**
+ * @file Terminal.hpp
+ * @brief Definition of the Terminal class for shell emulation.
+ *
+ * This class simulates terminal behavior and manages the command-line interface
+ * logic. It serves as the central controller for user-system interaction.
+ *
+ * Key Responsibilities:
+ * - Prompt Generation: Dynamically constructs the command-line prefix.
+ * - Input Parsing: Sanitizes and tokenizes raw user input.
+ * - Command Execution: Dispatches parsed commands to the respective logic
+ * handlers.
+ *
+ * The terminal operates as a state machine with the following states:
+ * - WAITING:   Idle state, awaiting the start of user interaction.
+ * - TYPING:    Active state, buffering real-time keyboard input.
+ * - EXECUTING: Processing state, running command logic and awaiting completion.
+ */
+#pragma once
+
+#include <string>
+namespace terminal {
+
+/**
+ * @enum terminal_state
+ * @brief Represents the current operational state of the terminal
+ */
+enum class terminal_state {
+  waiting,  /**< Idle state, awaiting new user interaction. */
+  typing,   /**< Active input mode, buffering keystrokes. */
+  executing /**< Processing a command, input might be locked */
+};
+
+/**
+ * @class terminal
+ * @brief Core logic controller for the virtual shell enviroment
+ */
+class terminal {
+public:
+  terminal();
+  ~terminal() = default;
+
+  /**
+   * @brief Dynamically constructs the command-line prefix string
+   * @return A string representing the Prompt
+   */
+  std::string generate_prompt();
+
+  /**
+   * @brief Executes a validated command
+   * @param command The command string to be processed
+   */
+  void execute_command(const std::string &command);
+
+private:
+  terminal_state
+      _current_state; /**< Current internal state of the state machine */
+};
+
+} // namespace terminal
